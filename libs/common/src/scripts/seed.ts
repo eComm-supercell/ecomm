9ce13d8348e5e1efb81ef5cb324ab49973e4a657
mustafaKamal-fe <gistcodee@gmail.com>
2023-12-16 03:40:18 +0300

    fix: disable seed scripts

--- libs/common/src/scripts/seed.ts
+++ libs/common/src/scripts/seed.ts
@@ -1,43 +1,43 @@
-import * as dotenv from 'dotenv';
-import { PrismaClient } from '@prisma/client';
-import * as argon from 'argon2';
-import Role from '../enums/role.enum';
+// import * as dotenv from 'dotenv';
+// import { PrismaClient } from '@prisma/client';
+// import * as argon from 'argon2';
+// import Role from '../enums/role.enum';
 
-//import { customSeed } from './customSeed';
-// import Role from '@app/common/enums/role.enum';
+// //import { customSeed } from './customSeed';
+// // import Role from '@app/common/enums/role.enum';
 
-if (require.main === module) {
-  dotenv.config();
+// if (require.main === module) {
+//   dotenv.config();
 
-  seed().catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
-}
+//   seed().catch((error) => {
+//     console.error(error);
+//     process.exit(1);
+//   });
+// }
 
-function seed() {
-  console.info('Seeding database...');
+// function seed() {
+//   console.info('Seeding database...');
 
-  // const client = new PrismaClient();
-  // await client.user.upsert({
-  //   where: { username: 'super' },
-  //   update: {},
-  //   create: {
-  //     username: 'super',
-  //     fname: 'Administrator',
-  //     password: await argon.hash('super123'),
-  //     role: Role.superAdmin,
-  //     wallet: {
-  //       create: {
-  //         amount: 0,
-  //       },
-  //     },
-  //   },
-  // });
-  // void client.$disconnect();
+//   // const client = new PrismaClient();
+//   // await client.user.upsert({
+//   //   where: { username: 'super' },
+//   //   update: {},
+//   //   create: {
+//   //     username: 'super',
+//   //     fname: 'Administrator',
+//   //     password: await argon.hash('super123'),
+//   //     role: Role.superAdmin,
+//   //     wallet: {
+//   //       create: {
+//   //         amount: 0,
+//   //       },
+//   //     },
+//   //   },
+//   // });
+//   // void client.$disconnect();
 
-  // console.info('Seeding database with custom seed...');
-  // customSeed();
+//   // console.info('Seeding database with custom seed...');
+//   // customSeed();
 
-  console.info('Seeded database successfully');
-}
+//   console.info('Seeded database successfully');
+// }
