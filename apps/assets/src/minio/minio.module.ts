a81bf8cf7c05f8e8e2a8f94d7ef64b27610d400a
mustafaKamal-fe <gistcodee@gmail.com>
2024-01-23 14:01:50 +0300

    wip

--- apps/assets/src/minio/minio.module.ts
+++ apps/assets/src/minio/minio.module.ts
@@ -12,7 +12,7 @@ import { MinioService } from './minio.service';
       useFactory: () => {
         const minioClient = new Minio.Client({
           endPoint: process.env.MINIO_ENDPOINT as string,
-          port: parseInt(process.env.MINIO_PORT as string),
+          // port: parseInt(process.env.MINIO_PORT as string),
           useSSL: false,
           accessKey: process.env.MINIO_ACCESS_KEY as string,
           secretKey: process.env.MINIO_SECRET_KEY as string,
